b3cbdc75c66d69e13d0400ba1ec3e7ff518963f3
Markus Fenske <iblue@gmx.net>
2013-09-24 11:59:37 +0200

    Added fonts, colorscheme, powerline

--- install.sh
+++ install.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
-cp -rf * ~
+# FIXME
+cp -rf home-dotfiles/* ~
 
 # Set zsh as shell
 chsh -s /bin/zsh
+
+# Install powerline patched fonts
+sudo cp -rf terminesspowerline /usr/share/fonts
+sudo mkfontscale /usr/share/fonts/terminesspowerline
+sudo mkfontdir /usr/share/fonts/terminesspowerline
+sudo fc-cache -f /usr/share/fonts/terminesspowerline
+# Works in fedora. May not work in other distros
+sudo ln -s /usr/share/fonts/terminesspowerline /etc/X11/fontpath.d/
+echo "Installed terminesspowerline. Please restart X. Make sure your urxvt has --enable-unicode3"
+
 # Set awesome as wm
