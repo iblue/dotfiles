ffc6a4fc1c107dfccfcb9a8e19f09570e5d7543b
Markus Fenske <iblue@gmx.net>
2013-09-24 12:46:43 +0200

    Fixed reinstall

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ sudo mkfontscale /usr/share/fonts/terminesspowerline
 sudo mkfontdir /usr/share/fonts/terminesspowerline
 sudo fc-cache -f /usr/share/fonts/terminesspowerline
 # Works in fedora. May not work in other distros
-sudo ln -s /usr/share/fonts/terminesspowerline /etc/X11/fontpath.d/
+sudo ln -sf /usr/share/fonts/terminesspowerline /etc/X11/fontpath.d/
 echo "Installed terminesspowerline. Please restart X. Make sure your urxvt has --enable-unicode3"
 
 # Set awesome as wm
